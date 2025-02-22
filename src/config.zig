1e8c38c4d02a1c27f154554e1cabdd48aebd0396
Lumor Sunil <lumorsunil@gmail.com>
2025-02-22 01:20:22 +0100

    Added assets

--- src/config.zig
+++ src/config.zig
@@ -1,4 +1,4 @@
-pub const assetsRoot = "D:\\studio\\busy-taxi\\";
+pub const assetsRoot = "assets/";
 pub const initialSize = .{ .w = 1200, .h = 1400 };
 pub const initialPosition = .{ .x = 1220, .y = 0 };
 pub const highScorePath = "highscore.json";
